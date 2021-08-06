99eaf2dab8baf75ce77f78c69656e9c1bf1ceea4
KingricharDV#0439 <northerncommunity1@gmail.com>
2021-08-06 13:12:59 -1000

    Update update 1MN.sh

--- Update Scripts/update 1MN.sh
+++ Update Scripts/update 1MN.sh
@@ -7,15 +7,16 @@ rm -rf nesteggd nestegg-cli nestegg-tx
 cd /root/
 wget https://github.com/KingricharVD/NestEGG/releases/download/v1.5.0.0/nestegglin1.5.0.0.tar.gz
 tar -xzf nestegglin1.5.0.0.tar.gz
-cp nestegglin1.5.0.0.tar.gz /usr/local/bin/
-cp nestegglin1.5.0.0.tar.gz /root/.nestegg/
-cp nestegglin1.5.0.0.tar.gz /root/
+rm -rf nestegglin1.5.0.0.tar.gz
 cd /root/.nestegg
+wget https://github.com/KingricharVD/NestEGG/releases/download/v1.5.0.0/nestegglin1.5.0.0.tar.gz
 tar -xzf nestegglin1.5.0.0.tar.gz
 rm -rf nestegglin1.5.0.0.tar.gz
 sudo chmod 755 -R  /root/.nestegg/nestegg*
 cd /usr/local/bin/
+wget https://github.com/KingricharVD/NestEGG/releases/download/v1.5.0.0/nestegglin1.5.0.0.tar.gz
 tar -xzf nestegglin1.5.0.0.tar.gz
+rm -rf nestegglin1.5.0.0.tar.gz
 sudo chmod 755 -R  /usr/local/bin/nestegg*
 sudo chmod 755 -R  /root/nestegg*
 cd /root/.nestegg
