8b2bee220c326ed5e0b4499a539ff7cd014bb858
KingricharDV#0439 <northerncommunity1@gmail.com>
2021-08-06 13:21:14 -1000

    Update update 1MN.sh

--- Update Scripts/update 1MN.sh
+++ Update Scripts/update 1MN.sh
@@ -18,7 +18,6 @@ cd /usr/local/bin/
 wget https://github.com/KingricharVD/NestEGG/releases/download/v1.5.0.0/nestegglin1.5.0.0.tar.gz
 tar -xzf nestegglin1.5.0.0.tar.gz
 rm -rf nestegglin1.5.0.0.tar.gz
-sudo chmod 755 -R  /usr/local/bin/nestegg*
 cd /root/.nestegg/
 rm -rf nestegglin1.5.0.0.tar.gz
 ./nesteggd -daemon -resync
