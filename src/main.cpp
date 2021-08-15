680af50cc8d2e1b80756568fafa36e15107cfa38
KingricharDV#0439 <northerncommunity1@gmail.com>
2021-08-14 15:14:08 -1000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -92,7 +92,7 @@ Mutex g_best_block_mutex;
 std::condition_variable g_best_block_cv;
 uint256 g_best_block;
 
-int nScriptCheckThreads = 0;
+int nScriptCheckThreads = 4;
 std::atomic<bool> fImporting{false};
 std::atomic<bool> fReindex{false};
 bool fTxIndex = true;
