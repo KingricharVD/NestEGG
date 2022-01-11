7ebe01c1278cc6d1a698ea335037711777fb50a7
KingricharDV#0439 <kingrichardv@protonmail.com>
2022-01-11 13:03:44 -1000

    Update keystore.cpp

--- src/keystore.cpp
+++ src/keystore.cpp
@@ -1,272 +1,137 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2014 The Bitcoin developers
 // Copyright (c) 2009-2014 The Bitcoin developers
 // Copyright (c) 2017-2019 The PIVX developers
-// Copyright (c) 2017-2019 The PIVX developers
-// Copyright (c) 2021 The DECENOMY Core Developers
 // Copyright (c) 2021 The DECENOMY Core Developers
 // Distributed under the MIT software license, see the accompanying
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #include "keystore.h"
-#include "keystore.h"
-#include "crypter.h"
 #include "crypter.h"
 #include "key.h"
-#include "key.h"
-#include "script/script.h"
 #include "script/script.h"
 #include "script/standard.h"
-#include "script/standard.h"
-#include "util.h"
 #include "util.h"
 bool CKeyStore::AddKey(const CKey& key)
-bool CKeyStore::AddKey(const CKey& key)
-{
 {
     return AddKeyPubKey(key, key.GetPubKey());
-    return AddKeyPubKey(key, key.GetPubKey());
-}
 }
 bool CBasicKeyStore::GetPubKey(const CKeyID &address, CPubKey &vchPubKeyOut) const
-bool CBasicKeyStore::GetPubKey(const CKeyID &address, CPubKey &vchPubKeyOut) const
-{
 {
     CKey key;
-    CKey key;
-    if (!GetKey(address, key)) {
     if (!GetKey(address, key)) {
         WatchKeyMap::const_iterator it = mapWatchKeys.find(address);
-        WatchKeyMap::const_iterator it = mapWatchKeys.find(address);
-        if (it != mapWatchKeys.end()) {
         if (it != mapWatchKeys.end()) {
             vchPubKeyOut = it->second;
-            vchPubKeyOut = it->second;
-            return true;
             return true;
         }
-        }
         return false;
-        return false;
-    }
     }
     vchPubKeyOut = key.GetPubKey();
-    vchPubKeyOut = key.GetPubKey();
     return true;
-    return true;
-}
 }
 bool CBasicKeyStore::AddKeyPubKey(const CKey& key, const CPubKey& pubkey)
-bool CBasicKeyStore::AddKeyPubKey(const CKey& key, const CPubKey& pubkey)
 {
-{
-    LOCK(cs_KeyStore);
     LOCK(cs_KeyStore);
     mapKeys[pubkey.GetID()] = key;
-    mapKeys[pubkey.GetID()] = key;
     return true;
-    return true;
-}
 }
 bool CBasicKeyStore::AddCScript(const CScript& redeemScript)
-bool CBasicKeyStore::AddCScript(const CScript& redeemScript)
-{
 {
     if (redeemScript.size() > MAX_SCRIPT_ELEMENT_SIZE)
-    if (redeemScript.size() > MAX_SCRIPT_ELEMENT_SIZE)
-        return error("CBasicKeyStore::AddCScript() : redeemScripts > %i bytes are invalid", MAX_SCRIPT_ELEMENT_SIZE);
         return error("CBasicKeyStore::AddCScript() : redeemScripts > %i bytes are invalid", MAX_SCRIPT_ELEMENT_SIZE);
     LOCK(cs_KeyStore);
-    LOCK(cs_KeyStore);
-    mapScripts[CScriptID(redeemScript)] = redeemScript;
     mapScripts[CScriptID(redeemScript)] = redeemScript;
     return true;
-    return true;
-}
 }
 bool CBasicKeyStore::HaveCScript(const CScriptID& hash) const
-bool CBasicKeyStore::HaveCScript(const CScriptID& hash) const
 {
-{
-    LOCK(cs_KeyStore);
     LOCK(cs_KeyStore);
     return mapScripts.count(hash) > 0;
-    return mapScripts.count(hash) > 0;
-}
 }
 bool CBasicKeyStore::GetCScript(const CScriptID& hash, CScript& redeemScriptOut) const
-bool CBasicKeyStore::GetCScript(const CScriptID& hash, CScript& redeemScriptOut) const
 {
-{
-    LOCK(cs_KeyStore);
     LOCK(cs_KeyStore);
     ScriptMap::const_iterator mi = mapScripts.find(hash);
-    ScriptMap::const_iterator mi = mapScripts.find(hash);
-    if (mi != mapScripts.end()) {
     if (mi != mapScripts.end()) {
         redeemScriptOut = (*mi).second;
-        redeemScriptOut = (*mi).second;
-        return true;
         return true;
     }
-    }
     return false;
-    return false;
-}
 }
 static bool ExtractPubKey(const CScript &dest, CPubKey& pubKeyOut)
-static bool ExtractPubKey(const CScript &dest, CPubKey& pubKeyOut)
-{
 {
     //TODO: Use Solver to extract this?
-    //TODO: Use Solver to extract this?
-    CScript::const_iterator pc = dest.begin();
     CScript::const_iterator pc = dest.begin();
     opcodetype opcode;
-    opcodetype opcode;
-    std::vector<unsigned char> vch;
     std::vector<unsigned char> vch;
     if (!dest.GetOp(pc, opcode, vch) || vch.size() < 33 || vch.size() > 65)
-    if (!dest.GetOp(pc, opcode, vch) || vch.size() < 33 || vch.size() > 65)
-        return false;
         return false;
     pubKeyOut = CPubKey(vch);
-    pubKeyOut = CPubKey(vch);
-    if (!pubKeyOut.IsFullyValid())
     if (!pubKeyOut.IsFullyValid())
         return false;
-        return false;
-    if (!dest.GetOp(pc, opcode, vch) || opcode != OP_CHECKSIG || dest.GetOp(pc, opcode, vch))
     if (!dest.GetOp(pc, opcode, vch) || opcode != OP_CHECKSIG || dest.GetOp(pc, opcode, vch))
         return false;
-        return false;
     return true;
-    return true;
-}
 }
 bool CBasicKeyStore::AddWatchOnly(const CScript& dest)
-bool CBasicKeyStore::AddWatchOnly(const CScript& dest)
 {
-{
-    LOCK(cs_KeyStore);
     LOCK(cs_KeyStore);
     setWatchOnly.insert(dest);
-    setWatchOnly.insert(dest);
     CPubKey pubKey;
-    CPubKey pubKey;
-    if (ExtractPubKey(dest, pubKey))
     if (ExtractPubKey(dest, pubKey))
         mapWatchKeys[pubKey.GetID()] = pubKey;
-        mapWatchKeys[pubKey.GetID()] = pubKey;
     return true;
-    return true;
-}
 }
 bool CBasicKeyStore::RemoveWatchOnly(const CScript& dest)
-bool CBasicKeyStore::RemoveWatchOnly(const CScript& dest)
 {
-{
-    LOCK(cs_KeyStore);
     LOCK(cs_KeyStore);
     setWatchOnly.erase(dest);
-    setWatchOnly.erase(dest);
-    CPubKey pubKey;
     CPubKey pubKey;
     if (ExtractPubKey(dest, pubKey))
-    if (ExtractPubKey(dest, pubKey))
-        mapWatchKeys.erase(pubKey.GetID());
         mapWatchKeys.erase(pubKey.GetID());
     return true;
-    return true;
-}
 }
 bool CBasicKeyStore::HaveWatchOnly(const CScript& dest) const
-bool CBasicKeyStore::HaveWatchOnly(const CScript& dest) const
 {
-{
-    LOCK(cs_KeyStore);
     LOCK(cs_KeyStore);
     return setWatchOnly.count(dest) > 0;
-    return setWatchOnly.count(dest) > 0;
-}
 }
 bool CBasicKeyStore::HaveWatchOnly() const
-bool CBasicKeyStore::HaveWatchOnly() const
-{
 {
     LOCK(cs_KeyStore);
-    LOCK(cs_KeyStore);
-    return (!setWatchOnly.empty());
     return (!setWatchOnly.empty());
 }
-}
-bool CBasicKeyStore::HaveKey(const CKeyID& address) const
 bool CBasicKeyStore::HaveKey(const CKeyID& address) const
 {
-{
-    bool result;
     bool result;
     {
-    {
-        LOCK(cs_KeyStore);
         LOCK(cs_KeyStore);
         result = (mapKeys.count(address) > 0);
-        result = (mapKeys.count(address) > 0);
-    }
     }
     return result;
-    return result;
-}
 }
 void CBasicKeyStore::GetKeys(std::set<CKeyID>& setAddress) const
-void CBasicKeyStore::GetKeys(std::set<CKeyID>& setAddress) const
-{
 {
     setAddress.clear();
-    setAddress.clear();
     {
-    {
-        LOCK(cs_KeyStore);
         LOCK(cs_KeyStore);
         KeyMap::const_iterator mi = mapKeys.begin();
-        KeyMap::const_iterator mi = mapKeys.begin();
-        while (mi != mapKeys.end()) {
         while (mi != mapKeys.end()) {
             setAddress.insert((*mi).first);
-            setAddress.insert((*mi).first);
-            mi++;
             mi++;
         }
-        }
     }
-    }
-}
 }
 bool CBasicKeyStore::GetKey(const CKeyID& address, CKey& keyOut) const
-bool CBasicKeyStore::GetKey(const CKeyID& address, CKey& keyOut) const
-{
 {
     {
-    {
-        LOCK(cs_KeyStore);
         LOCK(cs_KeyStore);
         KeyMap::const_iterator mi = mapKeys.find(address);
-        KeyMap::const_iterator mi = mapKeys.find(address);
-        if (mi != mapKeys.end()) {
         if (mi != mapKeys.end()) {
             keyOut = mi->second;
-            keyOut = mi->second;
-            return true;
             return true;
         }
-        }
-    }
     }
     return false;
-    return false;
-}
 }
 
 //! Sapling
