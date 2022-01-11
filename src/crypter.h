7bab6dea18614b31c88083a6fa7c314265a5dc29
KingricharDV#0439 <kingrichardv@protonmail.com>
2022-01-10 16:57:31 -1000

    Revert "update"
    This reverts commit 282cec479a4a320311ce9c06ab186964bc1b8375.

--- src/crypter.h
+++ src/crypter.h
@@ -21,13 +21,13 @@ const unsigned int WALLET_CRYPTO_IV_SIZE = 16;
 /**
  * Private key encryption is done based on a CMasterKey,
  * which holds a salt and random encryption key.
- *
+ * 
  * CMasterKeys are encrypted using AES-256-CBC using a key
  * derived using derivation method nDerivationMethod
  * (0 == EVP_sha512()) and derivation iterations nDeriveIterations.
  * vchOtherDerivationParameters is provided for alternative algorithms
  * which may require more parameters (such as scrypt).
- *
+ * 
  * Wallet Private Keys are then encrypted using AES-256-CBC
  * with the double-sha256 of the public key as the IV, and the
  * master key's key as the encryption key (see keystore.[ch]).
@@ -142,18 +142,13 @@ private:
 protected:
     // TODO: In the future, move this variable to the wallet class directly following upstream's structure.
     CKeyingMaterial vMasterKey;
-    // Sapling
-        CryptedSaplingSpendingKeyMap mapCryptedSaplingSpendingKeys;
-
+    
     bool SetCrypted();
 
     //! will encrypt previously unencrypted keys
     bool EncryptKeys(CKeyingMaterial& vMasterKeyIn);
 
     CryptedKeyMap mapCryptedKeys;
-
-    // Unlock Sapling keys
-   bool UnlockSaplingKeys(const CKeyingMaterial& vMasterKeyIn, bool fDecryptionThoroughlyChecked);
 
 public:
     CCryptoKeyStore() : fUseCrypto(false) { }
@@ -203,14 +198,6 @@ public:
             mi++;
         }
     }
-
-    //! Sapling
-        virtual bool AddCryptedSaplingSpendingKey(
-                const libzcash::SaplingExtendedFullViewingKey &extfvk,
-                const std::vector<unsigned char> &vchCryptedSecret,
-                const libzcash::SaplingPaymentAddress &defaultAddr);
-        bool HaveSaplingSpendingKey(const libzcash::SaplingFullViewingKey &fvk) const;
-        bool GetSaplingSpendingKey(const libzcash::SaplingFullViewingKey &fvk, libzcash::SaplingExtendedSpendingKey &skOut) const;
 
     /**
      * Wallet status (encrypted, locked) changed.
