2bf732d228e98785df7c2f3a8243ba0472b5b265
KingricharDV#0439 <kingrichardv@protonmail.com>
2022-02-25 13:56:58 -1000

    Update blockchain.cpp

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -134,7 +134,7 @@ UniValue blockToJSON(const CBlock& block, const CBlockIndex* blockindex, bool tx
         uint256 hashProofOfStakeRet;
         if (!GetStakeKernelHash(hashProofOfStakeRet, block, blockindex->pprev))
             throw JSONRPCError(RPC_INTERNAL_ERROR, "Cannot get proof of stake hash");
-        std::string stakeModifier = (Params().GetConsensus().NetworkUpgradeActive(blockindex->nHeight, Consensus::UPGRADE_V3_4) ?
+        std::string stakeModifier = (Params().GetConsensus().NetworkUpgradeActive(blockindex->nHeight, Consensus::UPGRADE_V4_0) ?
                                      blockindex->GetStakeModifierV2().GetHex() :
                                      strprintf("%016x", blockindex->GetStakeModifierV1()));
         result.push_back(Pair("stakeModifier", stakeModifier));
@@ -705,7 +705,7 @@ static UniValue SoftForkMajorityDesc(int version, const CBlockIndex* pindex, con
         idx = Consensus::UPGRADE_BIP65;
         break;
     case 6:
-        idx = Consensus::UPGRADE_V3_4;
+        idx = Consensus::UPGRADE_V4_0;
         break;
     case 7:
         idx = Consensus::UPGRADE_V4_0;
