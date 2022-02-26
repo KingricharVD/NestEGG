0777e466fb67371ee2a356755440f790bc8d7466
KingricharDV#0439 <kingrichardv@protonmail.com>
2022-02-25 14:02:21 -1000

    Update misc.cpp

--- src/rpc/misc.cpp
+++ src/rpc/misc.cpp
@@ -11,7 +11,6 @@
 #include "httpserver.h"
 #include "consensus/zerocoin_verify.h"
 #include "init.h"
-#include "sapling/key_io_sapling.h"
 #include "main.h"
 #include "masternode-sync.h"
 #include "net.h"
